d39c77c9517139854016df5401245ed4dd700ea3
Rajat Nigam <rajnigam@WKMZTCFF5574.global.publicisgroupe.net>
2021-12-18 22:03:35 +0530

    update petstore dir

--- ch11_CustomProvider/pet-provider/main.go
+++ ch11_CustomProvider/pet-provider/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/rajat965ng/terraform-in-action/tree/master/ch11_CustomProvider/pet-provider/petstore"
 )
 
 func main() {
